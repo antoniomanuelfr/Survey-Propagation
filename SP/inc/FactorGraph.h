1bc33563f959973bddb5676e3386a5c9a51cd749
Antonio Manuel Fresneda Rodriguez <antoniomanuelfr@gmail.com>
2020-03-26 23:17:10 +0100

    DIMACS decoder.

--- SP/inc/FactorGraph.h
+++ SP/inc/FactorGraph.h
@@ -10,22 +10,44 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-/*
- * @brief:
- *
- *
+#include <iterator>
+#include <sstream>
+
+/**
+ * @brief Class for handle the factor graph representation of a CNF formula.
  **/
 class FactorGraph{
 private:
-    std::vector<std::vector<int>> NegativeNodes;  /** < Vector that storages the postive literals.  */
-    std::vector<std::vector<int>> PositiveNodes;  /** < Vector that storages the negative literals. */
-    std::vector<std::vector<float>> EdgePis;      /** < Vector that storages the edge pis (weight). */
+    std::vector<std::vector<int>> PositiveNodes;  /**< Vector that storage the negative literals. */
+    std::vector<std::vector<int>> NegativeNodes;  /**< Vector that storage the positive literals.  */
+    std::vector<std::vector<float>> EdgePis;      /**< Vector that storage the edge pis (weight). */
 public:
     FactorGraph(const std::string& path);
-
-    const std::vector<std::vector<int>> &getNegativeNodes() const;
+    /**
+     * @brief Getter for PositveNodes vector.
+     * @return A constant reference to PositveNodes.
+     */
     const std::vector<std::vector<int>> &getPositiveNodes() const;
+    /**
+     * @brief Getter for NegativeNodes vector.
+     * @return A constant reference to NegativesNodes.
+     */
+    const std::vector<std::vector<int>> &getNegativeNodes() const;
+    /**
+     * @brief Getter for EdgePis vector.
+     * @return A constant reference to EdgePis.
+     */
     const std::vector<std::vector<float>> &getEdgePis() const;
-
+    /**
+     * @brief Function that reads a DIMACS file and overwrite the Positive and Negative vectors by it's content.
+     * @param String with the path to the file.
+     */
+    void ReadDIMACS(const std::string& path);
 };
+/**
+ * @brief Function that splits a string separated by spaces.
+ * @param str String that is going to be splitted.
+ * @return A vector<string> with the splits of the string.
+ */
+std::vector<std::string> SplitString(const std::string& str);
 #endif //FACTOR_GRAPH_H
--- SP/src/FactorGraph.cpp
+++ SP/src/FactorGraph.cpp
@@ -4,6 +4,14 @@
 
 #include "../inc/FactorGraph.h"
 
+std::vector<std::string> SplitString(const std::string& str)
+{
+    std::vector<std::string> cont;
+    std::istringstream aux(str);
+    std::copy(std::istream_iterator<std::string>(aux),std::istream_iterator<std::string>(), std::back_inserter(cont));
+    return cont;
+}
+
 const std::vector<std::vector<int>> &FactorGraph::getNegativeNodes() const {
     return NegativeNodes;
 }
@@ -17,18 +25,54 @@ const std::vector<std::vector<float>> &FactorGraph::getEdgePis() const {
 }
 
 FactorGraph::FactorGraph(const std::string& path) {
+    ReadDIMACS(path);
+}
+
+void FactorGraph::ReadDIMACS(const std::string &path) {
     std::ifstream input_file (path);
     std::string line;
     if (input_file.is_open()){
+        // Check if the vectors are not empty
+        if (!this->PositiveNodes.empty() || !this->NegativeNodes.empty() || !this->EdgePis.empty()) {
+            PositiveNodes.clear();
+            NegativeNodes.clear();
+        }
         // Skip the comments.
         while(getline(input_file, line) && line[0] == 'c');
-            std::cout << line[6] << std::endl;
-        if (line[0] == 'p'){
+        std::vector<std::string> split = SplitString(line);
+        if (split[0] == "p" && split[1] == "cnf"){
+            int n_variables = std::stoi(split[2]);
+            int n_clauses = std::stoi(split[3]);
+            int counter = 0, actual_value = 0, i;
+            std::vector<int> positive_adjacency_list, negative_adjacency_list;
+            std::vector<int>* selected_list;
+            while (getline(input_file,line) && counter < n_clauses) {
+                split = SplitString(line);
+                i = 0;
+                while(split[i] != "0" && i < n_variables) {
+                    actual_value = std::stoi(split[i]);
+                    selected_list = actual_value > 0 ? &positive_adjacency_list : &negative_adjacency_list;
+                    selected_list->push_back(abs(actual_value));
+                    i++;
+                }
+                //It's needed to push even when empty,
+                PositiveNodes.push_back(positive_adjacency_list);
+                NegativeNodes.push_back(negative_adjacency_list);
+
+                if (!positive_adjacency_list.empty()) {
+                    positive_adjacency_list.clear();
+                }
+                if(!negative_adjacency_list.empty()) {
+                    negative_adjacency_list.clear();
+                }
+                counter++;
+            }
         }else{
-            std::cout << "Enter a valid .cnf file." << std::endl;
+            std::cerr << "Enter a valid DIMACS file" << std::endl;
         }
     }else{
-        std::cout << "File not found" << std::endl;
+        std::cerr << "File not found" << std::endl;
     }
+    std::cout << "File: " << path << " readed correctly." << std::endl;
 }
 

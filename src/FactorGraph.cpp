05e2b6ae459a25f6824fc8eccd17264eb44da99f
Antonio Fresneda <antoniomanuelfr@gmail.com>
2020-03-30 22:07:27 +0200

    Add function to print, improve comments and skip comments

--- src/FactorGraph.cpp
+++ src/FactorGraph.cpp
@@ -26,55 +26,64 @@ void FactorGraph::ReadDIMACS(const std::string &path, int& n_clauses, int& n_var
 
         // Skip the comments.
         while(getline(input_file, line) && line[0] == 'c');
-
+        // Split the string
         std::vector<std::string> split = SplitString(line);
+        // Check if the first line after the comments is the DIMACS header.
         if (split[0] == "p" && split[1] == "cnf") {
             n_variables = std::stoi(split[2]);
             n_clauses = std::stoi(split[3]);
             int counter = 0, actual_value = 0, i;
+            // This are auxiliary vectors that stores the adjacency list of the actual clause
             std::vector<int> positive_adjacency_vector, negative_adjacency_vector;
-            std::vector<int>* selected_vector;
+            std::vector<int>* selected_variables_vector; // This pointer helps to not repeat code.
+            // This are auxiliary vectors that stores the clauses where appear the actual variable
             std::vector<std::vector<int>> positive_clauses(n_variables), negative_clauses(n_variables);
-            std::vector<std::vector<int>>* selected_clauses_vector;
-            
+            std::vector<std::vector<int>>* selected_clauses_vector; // The same as the above pointer
+            // We start reading the clauses section of the file
             while (getline(input_file,line) && counter < n_clauses) {
                 split = SplitString(line);
-                i = 0;
-                while(split[i] != "0" && i < n_variables) {
-                    actual_value = std::stoi(split[i]);
-                    if (actual_value > 0){
-                        selected_vector= &positive_adjacency_vector;
-                        selected_clauses_vector = &positive_clauses;
-                    } else{
-                        selected_vector= &negative_adjacency_vector;
-                        selected_clauses_vector = &negative_clauses;
+                // We process only the line that are not comments
+                if (split[0] != "c") {
+                    i = 0;
+                    // We start processing the actual clause using the split vector
+                    while (split[i] != "0" && i < n_variables) {
+                        actual_value = std::stoi(split[i]);
+                        // If the actual value is positive we need to storage the info in the right vector
+                        if (actual_value > 0) {
+                            selected_variables_vector = &positive_adjacency_vector;
+                            selected_clauses_vector = &positive_clauses;
+                        // If the actual value is positive we need to storage the info in the right vector
+                        } else {
+                            selected_variables_vector = &negative_adjacency_vector;
+                            selected_clauses_vector = &negative_clauses;
+                        }
+                        // We save the information in the right vectors.
+                        selected_variables_vector->push_back(abs(actual_value));
+                        (*selected_clauses_vector)[abs(actual_value) - 1].push_back(counter);
+                        i++;
                     }
-                    selected_vector->push_back(abs(actual_value));
-                    (*selected_clauses_vector)[abs(actual_value)-1].push_back(counter);
-                    i++;
-                }
-                //It's needed to push even when empty,
-                this->PositiveVariables.push_back(positive_adjacency_vector);
-                this->NegativeVariables.push_back(negative_adjacency_vector);
+                    //It's needed to push even when empty,
+                    this->PositiveVariables.push_back(positive_adjacency_vector);
+                    this->NegativeVariables.push_back(negative_adjacency_vector);
 
-                if (!positive_adjacency_vector.empty()) {
-                    positive_adjacency_vector.clear();
+                    if (!positive_adjacency_vector.empty()) {
+                        positive_adjacency_vector.clear();
+                    }
+                    if (!negative_adjacency_vector.empty()) {
+                        negative_adjacency_vector.clear();
+                    }
+                    counter++;
                 }
-                if(!negative_adjacency_vector.empty()) {
-                    negative_adjacency_vector.clear();
-                }
-                counter++;
             }
             this->PositiveClauses = positive_clauses;
             this->NegativeClauses = negative_clauses;
-
         }else{
             std::cerr << "Enter a valid DIMACS file" << std::endl;
         }
     }else{
         std::cerr << "File not found" << std::endl;
     }
-    std::cout << "File: " << path << " readed correctly." << std::endl;
+    std::cout << "File: " << path << " read correctly." << std::endl;
 }
 
 void FactorGraph::VariablesInClause(int clause, std::vector<int>& positives, std::vector<int>& negatives) const {
@@ -83,7 +92,7 @@ void FactorGraph::VariablesInClause(int clause, std::vector<int>& positives, std
 
     if(!negatives.empty())
         negatives.clear();
-
+    //
     positives.insert(positives.begin(), this->PositiveVariables[clause].cbegin(),
             this->PositiveVariables[clause].cend());
     negatives.insert(negatives.begin(), this->NegativeVariables[clause].cbegin(),
@@ -94,6 +103,7 @@ int FactorGraph::Connection(unsigned int clause, unsigned int variable, bool &po
     auto it = std::find(this->PositiveVariables[clause].cbegin(), this->PositiveVariables[clause].cend(),
             variable);
     auto pos = -1;
+    // If the variable was founded, we have to find the index
     if(this->PositiveVariables[clause].cend() != it) {
         // The complexity of this function is constant if the iterators are random access iterators
         pos = std::distance(this->PositiveVariables[clause].cbegin(), it);
@@ -114,10 +124,9 @@ void FactorGraph::LoadEdgeWeights(bool rand, unsigned long seed) {
     if (!this->EdgeWeights.empty()) {
         this->EdgeWeights.clear();
     }
-
-    std::default_random_engine generator(seed);
-    std::uniform_real_distribution<double> distribution(0,1);
-
+    std::default_random_engine generator(seed); // Random engine generator.
+    std::uniform_real_distribution<double> distribution(0,1); //Distribution for the random generator.
+    // Reserve memory
     this->EdgeWeights.resize(this->NumberClauses);
     int pos;
     bool positive;
@@ -125,10 +134,12 @@ void FactorGraph::LoadEdgeWeights(bool rand, unsigned long seed) {
         for(int j = 0; j < this->NumberVariables; j++) {
             // j will be the index of the variable j+1
             pos = this->Connection(i, j+1, positive);
+            // If there is a positive connection between i clause and j variable
             if(pos !=-1 && positive) {
                 double value = rand ? (distribution(generator)) : 1.0;
                 this->EdgeWeights[i].push_back(value);
             } else {
+                // If there is a negative connection between i clause and j variable
                 if (pos != -1 && !positive) {
                     double value = rand ? distribution(generator) : 1.0;
                     this->EdgeWeights[i].push_back(value);
@@ -138,9 +149,24 @@ void FactorGraph::LoadEdgeWeights(bool rand, unsigned long seed) {
     }
 }
 
+void FactorGraph::PrintFactorGraph() {
+
+    for(int i = 0; i < this->PositiveVariables.size(); i++){
+        for (int it : this->PositiveVariables[i])
+            std::cout << it << " " ;
+
+        for (int it : this->NegativeVariables[i])
+            std::cout << -it << " " ;
+
+        std::cout << std::endl;
+    }
+}
+
 std::vector<std::string> SplitString(const std::string& str, char delim) {
+    // Vector with the sub-strings
     std::vector<std::string> cont;
     std::size_t current, previous = 0;
+    // Find the first delimiter
     current = str.find(delim);
     while (current != std::string::npos) {
         cont.push_back(str.substr(previous, current - previous));
